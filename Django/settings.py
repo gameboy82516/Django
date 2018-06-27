d12e0fab5e4c9632415a0bd2f6a11610be726f0d
Kao <gameboy82516@gmail.com>
2018-06-27 16:28:10 +0800

    Change setting

--- Django/settings.py
+++ Django/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+'''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), # Revised it.
 )
-
+'''
 #ALLOWED_HOSTS=['192.168.1.173']
